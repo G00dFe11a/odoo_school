1d76d11b40fca37baeb6b6de8b9041bd8e618253
alex <oleksandrzaglada@gmail.com>
2024-12-18 15:56:26 +0000

    Initial commit

---
+++ repositories/odoo_school/odoo_school_library/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Odoo School Library',
+    'author': 'Odoo School',
+    'website': 'https://odoo.school/',
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '17.0.1.0.0',
+    'images': ['static/description/icon.png'],
+    'depends': [
+        'base'
+    ],
+
+    'external_dependencies': {
+    'python': []
+    },
+
+    'data': [
+    ],
+    'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False
+}
