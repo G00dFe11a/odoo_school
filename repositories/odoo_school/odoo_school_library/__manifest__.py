1dc0e2a4de5d45db2ec69c436748dff3dba8bf0e
alex <oleksandrzaglada@gmail.com>
2024-12-19 15:36:05 +0000

    add some new info

--- repositories/odoo_school/odoo_school_library/__manifest__.py
+++ repositories/odoo_school/odoo_school_library/__manifest__.py
@@ -15,8 +15,13 @@
     },
 
     'data': [
+        'security/ir.model.access.csv',
+        'views/odoo_school_library_menu.xml',
+        'views/odoo_school_library_book_views.xml',
     ],
     'demo': [
+        'demo/res_partner_demo.xml',
+        'demo/odoo.school.library.book.csv',
     ],
 
     'installable': True,
